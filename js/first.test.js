38081a41ac71010e7ec261b5690c495a5d191044
Krishna Bashaboina <Krishnarao.bashaboina@gmail.com>
2017-06-13 17:11:24 -0500

    Karma test

---
+++ js/first.test.js
@@ -0,0 +1,13 @@
+
+describe('test group', () => {
+    
+    it('test case 1: 2*2 is 4', () => {
+        expect( 2*2).toBe(4);
+    });
+        
+        
+    it('fail test case 1: 2*2 is 3', () => {
+         expect( 2*2).toBe(3);
+    });
+});
+    
